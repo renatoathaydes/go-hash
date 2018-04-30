9012a6a1ff50f4944e0f8b9379000708ac157ee3
renatoathaydes <renatoathaydes@hotmail.com>
2018-04-30 17:45:35 +0200

    Documented mobile API.

--- mobileapi/api.go
+++ mobileapi/api.go
@@ -6,28 +6,33 @@ import (
 	"github.com/renatoathaydes/go-hash/gohash_db"
 )
 
+// GroupIterator allows iterating over all groups in the database.
 type GroupIterator struct {
 	state        gohash_db.State
 	keys         []string
 	currentIndex uint
 }
 
+// EntryIterator allows iterating over all LoginInfo entries in a group.
 type EntryIterator struct {
 	Group        string
 	entries      []gohash_db.LoginInfo
 	currentIndex uint
 }
 
+// Entry is a wrapper around a LoginInfo instance.
+// LoginInfo cannot be exposed directly due to the gomobile tool's limitations.
 type Entry struct {
 	loginInfo gohash_db.LoginInfo
 }
 
-// Database is good
+// Database a go-hash database.
 type Database struct {
 	FileName string
 	state    gohash_db.State
 }
 
+// Next returns the next EntryIterator, if any, or nil if none is available.
 func (iter *GroupIterator) Next() *EntryIterator {
 	if len(iter.keys) < int(iter.currentIndex) {
 		group := iter.keys[iter.currentIndex]
@@ -38,6 +43,7 @@ func (iter *GroupIterator) Next() *EntryIterator {
 	return nil
 }
 
+// Next returns the next Entry, if any, or nil if none is available.
 func (iter *EntryIterator) Next() *Entry {
 	if len(iter.entries) < int(iter.currentIndex) {
 		info := iter.entries[iter.currentIndex]
@@ -47,30 +53,38 @@ func (iter *EntryIterator) Next() *Entry {
 	return nil
 }
 
+// Name of this Entry.
 func (entry *Entry) Name() string {
 	return entry.loginInfo.Name
 }
 
+// Username of this Entry.
 func (entry *Entry) Username() string {
 	return entry.loginInfo.Username
 }
 
+// Description of this Entry.
 func (entry *Entry) Description() string {
 	return entry.loginInfo.Description
 }
 
+// Password of this Entry.
 func (entry *Entry) Password() string {
 	return entry.loginInfo.Password
 }
 
+// Url of this Entry.
 func (entry *Entry) Url() string {
 	return entry.loginInfo.URL
 }
 
+// UpdatedAt the instance at which this entry was last updated.
+// As epoch-milliseconds since 1970-01-01T00:00:00.
 func (entry *Entry) UpdatedAt() int64 {
 	return entry.loginInfo.UpdatedAt.Unix()
 }
 
+// Iter returns a GroupIterator which can be used to iterate over the groups in this database.
 func (db *Database) Iter() *GroupIterator {
 	keys := make([]string, len(db.state))
 	i := 0
@@ -82,11 +96,11 @@ func (db *Database) Iter() *GroupIterator {
 	return &GroupIterator{keys: keys, state: db.state}
 }
 
-// ReadDatabase reads stuff
+// ReadDatabase reads a go-hash database.
 func ReadDatabase(filePath, password string) (*Database, error) {
 	state, err := gohash_db.ReadDatabase(filePath, password)
 	if err != nil {
-		return &Database{}, err
+		return nil, err
 	}
 	return &Database{FileName: filePath, state: state}, nil
 }
